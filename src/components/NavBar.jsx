28d2a3a3fdae0c1a4207ca2a050f02c2b3143f11
Mintu Sikder <mintusikder15@gmail.com>
2024-09-10 22:35:37 +0600

    added navbar section

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,60 @@
-
+import { Link, NavLink } from "react-router-dom";
 
 const NavBar = () => {
-    return (
-        <div>
-            
+  const nav = (
+    <>
+      <li>
+        <NavLink to="/">Home</NavLink>
+      </li>
+      <li>
+        <NavLink to="/products">Products</NavLink>
+      </li>
+      <li>
+        <NavLink to="/blog">Blog</NavLink>
+      </li>
+      <li>
+        <NavLink to="/contact">Contact</NavLink>
+      </li>
+   
+    </>
+  );
+  return (
+    <div className="navbar bg-base-100">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+          >
+            {nav}
+          </ul>
         </div>
-    );
+        <Link to="/" className="btn btn-ghost text-xl">GLASSES</Link>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">{nav}</ul>
+      </div>
+      <div className="navbar-end">
+        <a className="btn">Login</a>
+      </div>
+    </div>
+  );
 };
 
 export default NavBar;
